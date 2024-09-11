b89f515291ddf35d680be5e8b7d44e117e187bcc
Gerónimo Belfiori <gerobelcar@gmail.com>
2024-09-11 18:46:11 -0300

    Update Pagina Principal.js

--- Pagina Principal.js
+++ Pagina Principal.js
@@ -1,15 +1,1 @@
-// Obtén todos los enlaces del menú
-const links = document.querySelectorAll('.Navbar a');
 
-// Agrega un event listener a cada enlace
-links.forEach(link => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault(); // Evita el comportamiento por defecto del enlace
-
-    const targetId = link.getAttribute('href').substring(1);
-    const targetElement = document.getElementById(targetId);
-
-    // Desplázate suavemente hasta la sección
-    targetElement.scrollIntoView({ behavior: 'smooth' });
-  });
-});
